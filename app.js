4dc53eed272fee5933315fb7c2b01ac14fab0fc2
Adri Forczek <forczekadri@gmail.com>
2020-06-27 19:56:56 +0100

    feature: adds tetronimos

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
 // fires when the initial HTML document completely loaded
 document.addEventListener('DOMContentLoaded', () => {
+    // build grid
+    const grid = document.querySelector(".grid");
+    buildGrid(grid);
 
-    buildGrid();
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const ScoreDisplay = document.querySelector('#score');
+    const StartButton = document.querySelector('#start-button');
+    const width = 10
 
+    // Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zTetromino = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ]
+
+    const tTetromino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+2],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetronimo = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetronimo = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const tetronimoes = [lTetromino, zTetromino, tTetromino, oTetronimo, iTetronimo];
 })
 
-function buildGrid() {
-    let grid = document.getElementById("grid");
+function buildGrid(grid) {
     for(let i = 0; i < 200; i++) {
         let div = document.createElement("div");
         grid.appendChild(div);
